847f764482ed189610505c73a01d24e463f03d07
PAlamirpour <P.Alamirpour@gmail.com>
2020-04-20 17:21:25 +0000

    Remove wxWidgetsSrc folder

--- wxWidgetsSrc/wxWidgets-3.1.3/gtk-build/.deps/baselib_extended.o.d
+++
@@ -1,6 +0,0 @@
-baselib_extended.o: ../src/common/extended.c ../include/wx/defs.h \
- ../include/wx/platform.h ../include/wx/compiler.h \
- /home/pooya/Projects/udacity/CppND-Memory-Management-Chatbot/wxWidgetsSrc/wxWidgets-3.1.3/gtk-build/lib/wx/include/gtk3-unicode-static-3.1/wx/setup.h \
- ../include/wx/chkconf.h ../include/wx/unix/chkconf.h \
- ../include/wx/version.h ../include/wx/cpp.h ../include/wx/dlimpexp.h \
- ../include/wx/types.h ../include/wx/features.h ../include/wx/math.h
--- wxWidgetsSrc/wxWidgets-3.1.3/gtk-build/.deps/baselib_xti.o.d
+++
@@ -1,7 +0,0 @@
-baselib_xti.o: ../src/common/xti.cpp ../include/wx/wxprec.h \
- ../include/wx/defs.h ../include/wx/platform.h ../include/wx/compiler.h \
- /home/pooya/Projects/udacity/CppND-Memory-Management-Chatbot/wxWidgetsSrc/wxWidgets-3.1.3/gtk-build/lib/wx/include/gtk3-unicode-static-3.1/wx/setup.h \
- ../include/wx/chkconf.h ../include/wx/unix/chkconf.h \
- ../include/wx/version.h ../include/wx/cpp.h ../include/wx/dlimpexp.h \
- ../include/wx/types.h ../include/wx/debug.h ../include/wx/chartype.h \
- ../include/wx/features.h
